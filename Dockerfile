21fbf7e4d873afb3c027fc5387ebfe10d89c6f0e
cao.nvd@itsstraining.edu.vn <cao.nvd@itsstraining.edu.vn>
2020-05-19 20:37:39 +0700

    done upload image

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 FROM python
 ENV FLASK_APP app.py
+ENV FLASK_ENV development
+ENV FLASK_DEBUG true
 ENV FLASK_RUN_HOST 0.0.0.0
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
+RUN apt-get update
+RUN apt-get install -y tesseract-ocr
+RUN apt-get install -y libtesseract-dev
 COPY . .
 CMD ["flask", "run"]
--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, send_file
+import sys
+import pytesseract
+import os
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    print(pytesseract.get_tesseract_version(), file=sys.stderr)
+    return 'OK'
+    
+@app.route('/upload-image', methods = ['POST'] )
+def upload_image():
+    if request.method == 'POST':
+        f = request.files['image']
+        f.save(os.path.join("Image-input", f.filename))
+        return "OK"
+
 
 
 if __name__ == "__main__":
