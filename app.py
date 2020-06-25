8774bc05bc3d0893abb4dda4a39d5193e40151f0
cao.nvd@itsstraining.edu.vn <cao.nvd@itsstraining.edu.vn>
2020-06-25 07:32:26 +0700

    add api get image with send file

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, send_from_directory
 import sys
 import pytesseract
 import os
 import module_process_image
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./vision-key.json"
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/Image-output')
 
+app.config['CLIENT_IMAGE'] = "./Image-output"
 
 @app.route("/")
 def hello():
@@ -21,9 +22,14 @@ def upload_image():
         texts, boudingbox = module_process_image.detect_text(image_path)
         save_path = module_process_image.visualize(image_path, texts)
         print("save-path API-"+save_path)
-        return send_file(save_path)
+        return save_path
 
 
+@app.route('/image/<name_image>', methods=['GET'])
+def get_image(name_image):
+    if(request).method == 'GET':
+        #filename = f"{name_image}"
+        return send_from_directory(app.config['CLIENT_IMAGE'], filename= name_image, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
